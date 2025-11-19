3c53ebd2d554d4fa27ee39ff4f69f7e83a5d0d17
baozi05 <1374231610@qq.com>
2025-11-19 22:32:37 +0800

    时间自动填充工具类

---
+++ src/main/java/org/example/examsystem/utils/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package org.example.examsystem.utils;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * entity 类中 fill 的配置，保证能自动填充创建、更新的时间
+ */
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+}
