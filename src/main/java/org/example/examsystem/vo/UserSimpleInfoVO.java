78268b554a7b04d1d9880e7d828efae89148deef
baozi05 <1374231610@qq.com>
2025-12-01 16:51:03 +0800

    用户基本信息VO

---
+++ src/main/java/org/example/examsystem/vo/UserSimpleInfoVO.java
@@ -0,0 +1,14 @@
+package org.example.examsystem.vo;
+
+import lombok.Data;
+
+/**
+ * 用户基本信息VO
+ */
+@Data
+
+public class UserSimpleInfoVO {
+
+    private Long userId;
+    private String realName;
+}
