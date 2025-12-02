e755b9b36d9444a3113a66f3a07c792e86e91a8a
baozi05 <1374231610@qq.com>
2025-12-02 21:58:09 +0800

    基本类

---
+++ src/main/java/org/example/examsystem/entity/QuestionAnswer.java
@@ -0,0 +1,30 @@
+package org.example.examsystem.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@TableName("question_answer")
+@Data
+public class QuestionAnswer {
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 关联问题ID
+     */
+    private Long questionId;
+
+    /**
+     * 正确答案
+     */
+    private String correctAnswer;
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+}
